011693582ddab4f5a39933a6980da3c059d2030f
rudnine <st3yohan@gmail.com>
2016-08-31 00:02:37 +0900

    static test

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+/*
 app.get('/',function(req,res){
   res.send('Hello World!');
 });
+*/
 
 app.listen(3000, function(){
   console.log('Server On!');
